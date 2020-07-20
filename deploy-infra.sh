69a93e3a986c25be6780da1618563851d3a05be1
Daniel Lobo <daniel.lobo.182@gmail.com>
2020-07-20 13:04:26 -0300

    Change Hello World to Hello Cloud

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -53,5 +53,5 @@ aws cloudformation deploy \
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
     --profile awsbootstrap \
-    --query "Exports[?Name=='InstanceDNS'].Value"
+    --query "Exports[?Name=='InstanceEndpoint'].Value"
 fi
